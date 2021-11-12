639b9b57144a854621488727d91579ae7e792fb9
HernadiB <hernadi.barnabas@njit.hu>
2021-11-12 11:09:05 +0100

    feladat v1

---
+++ elso_laravel/resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Első</title>
+</head>
+<body>
+<h1>Főoldal</h1>
+<ul>
+    <li><a href="/doctor-house">Doctor House</a></li>
+    <li><a href="/uvegtigris/csoki">Üvegtigris - Csoki</a></li>
+    <li><a href="/uvegtigris/lali">Üvegtigris - Lali</a></li>
+    <li><a href="/modern-family">Modern-family</a></li>
+    <li><a href="/harry-potter/fred-es-george">harry-potter.fred-es-george</a></li>
+    <li><a href="/harry-potter/hermione">harry-potter.hermione</a></li>
+</ul>
+<ul>
+    <li><a href="/naptar/ma">Mai dátum</a></li>
+</ul>
+</body>
+</html>
--- elso_laravel/routes/web.php
+++ elso_laravel/routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/doctor-house', function () {
+    return "Nemcsak az emberek megalázásával lehet a gőzt kiereszteni; mondják,
+    hogy a bowling jobb még ennél is.";
+});
+
+Route::get('/uvegtigris/csoki', function () {
+    return "Mennyire vagy túsz? Sörhöz odaférsz?";
+});
+
+Route::get('/uvegtigris/lali', function () {
+    return "Mennyire vagy túsz? Sörhöz odaférsz?";
+});
+
+Route::get('/modern-family', function () {
+    return "A siker mindig 1 százalék ihlet, plusz 98 százalék verejték, végül
+    pedig 2 százalék odafigyelés.";
+});
+
+Route::get('/harry-potter/fred-es-george', function () {
+    return "– Mindig is tudtuk hol a határ - bólintott Fred <br>
+    - És csak óvatosan léptük át - tette hozzá George.";
+});
+
+Route::get('/harry-potter/hermione', function () {
+    return "Még egy ilyen remek ötlet, és mindhárman meghalunk, vagy akár ki
+    is csaphatnak!";
+});
+
+Route::get('/naptar/ma', function () {
+    return ;
 });
