35aad00eaebacc3a06c731ec6387e6ddfeb93b97
HernadiB <hernadi.barnabas@njit.hu>
2021-11-14 09:50:57 +0100

    mezga paula

---
+++ elso_laravel/resources/views/mezga-paula.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mézga Paula</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center bg-primary">Mézga Paula</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <?php $url = asset('images/mezga/mezgapaula.jpg')?>
+                <img src="<?= $url?>" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <p>Mézga Gézáné, születési neve: Rezovits Paula, Géza felesége. Igen jó háziasszony és gondoskodóan nevelgeti a csemetéit. Folyton panaszkodik a férjének valami miatt, ezért általában ekkor szabadul el a pokol MZ/X küldeménye kapcsán. Az első széria filmjei végén folyton azt mondja, hogy „Bárcsak a Hufnágel Pistihez mentem volna feleségül!” A harmadik szériában kiábrándul Pistiből, és ekkor azt mondja, hogy „Soha nem akarom hallani ezt a nevet”. Gyakran úgy tűnik, hogy a családban ő hordja a nadrágot, mivel általában basáskodik a férje fölött, aki megtörik az ő akaratának. Többször titulálja Gézát szerencsétlennek, pipogyának, lelketlennek, brutálisnak, ámokfutónak, attól függően, hogy a férje milyen baklövést követ el. Paula kezeli a családi kasszát is. Ennek ellenére azonban a végletekig kitart a családja mellett, ha a szükség úgy hozza. Az ő rokonsága valahol a balatoni Kecerakoncán lakik, ahová Mézgáék egyszer egy esküvőre mennek. . Hangja Győri Ilona volt (aki szintén a Balaton vidékéről származott), ő azonban 2001-ben meghalt. A negyedik sorozatban a helyét a hozzá hasonló orgánumú Tóth Judit vette át.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
