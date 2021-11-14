695ec2ee075ae4a528778852362859693a5e52d3
HernadiB <hernadi.barnabas@njit.hu>
2021-11-14 10:36:31 +0100

    mezga family

--- elso_laravel/resources/views/mezga.blade.php
+++ elso_laravel/resources/views/mezga.blade.php
@@ -10,13 +10,15 @@
 
 </head>
 <body>
-<h1>Mézga Család</h1>
-<p>A Mézga család az egyik legnépszerűbb magyar televíziós rajzfilmsorozat volt az 1960-as, '70-es és '80-as években. A 2004-es ismétlés alkalmával is mintegy félmillióan nézték. A sorozatot Németországban, Kubában, Bulgáriában, Olaszországban, Franciaországban, Csehországban és Szlovákiában is nagy sikerrel sugározták.</p>
-<h2>Családtagok</h2>
-<a class="btn btn-primary" href="<?=route("mezga.geza")?>">Mézga Géza</a>
-<a class="btn btn-success" href="<?=route("mezga.paula")?>">Mézga Paula</a>
-<a class="btn btn-secondary" href="<?=route("mezga.kriszta")?>">Mézga Kriszta</a>
-<a class="btn btn-danger" href="<?=route("mezga.aladar")?>">Mézga Aladár</a>
-<a class="btn btn-dark" href="<?=route("mezga.MZ")?>">MZ/X</a>
+<div class="container">
+    <h1>Mézga Család</h1>
+    <p>A Mézga család az egyik legnépszerűbb magyar televíziós rajzfilmsorozat volt az 1960-as, '70-es és '80-as években. A 2004-es ismétlés alkalmával is mintegy félmillióan nézték. A sorozatot Németországban, Kubában, Bulgáriában, Olaszországban, Franciaországban, Csehországban és Szlovákiában is nagy sikerrel sugározták.</p>
+    <h2>Családtagok</h2>
+    <a class="btn btn-primary" href="<?=route("mezga.geza")?>">Mézga Géza</a>
+    <a class="btn btn-success" href="<?=route("mezga.paula")?>">Mézga Paula</a>
+    <a class="btn btn-secondary" href="<?=route("mezga.kriszta")?>">Mézga Kriszta</a>
+    <a class="btn btn-danger" href="<?=route("mezga.aladar")?>">Mézga Aladár</a>
+    <a class="btn btn-dark" href="<?=route("mezga.MZ")?>">MZ/X</a>
+</div>
 </body>
 </html>
